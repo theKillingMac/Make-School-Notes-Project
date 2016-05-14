1c788f1126e501ab994f7c1a1aa0aa4b6e4a9715
Parth Shah <parths.97@gmail.com>
2016-05-14 23:54:32 +0530

    Add Local Saving using  Realm

--- MakeSchoolNotes/ViewControllers/NotesViewController.swift
+++ MakeSchoolNotes/ViewControllers/NotesViewController.swift
@@ -8,22 +8,54 @@
 //
 
 import UIKit
+import RealmSwift
 
 class NotesViewController: UITableViewController {
  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.dataSource = self
+    var notes: Results<Note>! {
+        didSet {
+            // Whenever notes update, update the table view
+            tableView?.reloadData()
+        }
+    }
     
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        
+        let myNote = Note()
+        myNote.title   = "Super Simple Test Note"
+        myNote.content = "A long piece of content"
+        
+        do {
+            // Before you can add it to Realm you must first grab the default Realm.
+            let realm = try Realm()
+            
+            // All changes to an object (addition, modification and deletion)
+            // must be done within a write transaction/closure.
+            try realm.write() {
+                // Add your new note to Realm
+                realm.add(myNote)
+            }
+            
+            //update our notes variable to contain all of the same data as our Realm database.
+            notes = realm.objects(Note)
+            
+        } catch {
+            print("handle error")
+        }
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        }
+
+    }
 
 extension NotesViewController {
     
@@ -31,14 +63,24 @@ extension NotesViewController {
         //This line makes a UITableViewCell with a unique identifier of "NoteCell".
         let cell = tableView.dequeueReusableCellWithIdentifier("NoteCell", forIndexPath: indexPath) as! NoteTableViewCell
         
-        cell.titleLabel.text = "Hello"
-        cell.dateLabel.text = "Today"
+        let row = indexPath.row
+        let note = notes[row] as Note
+        cell.note = note
         
         return cell
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        //if notes isn't empty, numberOfRowsInSection will return notes.count; otherwise it will return 0.
+        return notes?.count ?? 0
+        
+        //The above line is the same as:
+        // if let notes = notes {
+        //     return Int(notes.count)
+        // } else {
+        //     return 0
+        // }
+
     }
     
 }
--- MakeSchoolNotes/Views/NoteTableViewCell.swift
+++ MakeSchoolNotes/Views/NoteTableViewCell.swift
@@ -11,8 +11,24 @@ import UIKit
 
 class NoteTableViewCell: UITableViewCell {
     
-    //Title and Data labels
+    // initialize the date formatter only once, using a static computed property
+    static var dateFormatter: NSDateFormatter = {
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
-  
+    
+    var note: Note? {
+        didSet {
+            if let note = note, titleLabel = titleLabel, dateLabel = dateLabel {
+                titleLabel.text = note.title
+                dateLabel.text = NoteTableViewCell.dateFormatter.stringFromDate(note.modificationDate)
+            }
+        }
+    }
+    
+    
 }
